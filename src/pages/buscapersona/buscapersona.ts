140d0701741284da46aa1ef0be0c1ac03199518d
wcadena <wcadena@outlook.com>
2017-10-26 20:27:34 -0500

    Aumento de busqueda solo como plantilla

--- src/pages/buscapersona/buscapersona.ts
+++ src/pages/buscapersona/buscapersona.ts
@@ -16,9 +16,26 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class BuscapersonaPage {
 
-
+  myInput:any = {
+    nombre:"Wagner Cadena",
+    imagen:"http://inventario3.aerogal.dev/img/user2-160x160.jpg",
+    cargo: "Especialista de soporte en sitio",
+    ext: "1045"
+}
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
+  onInput(){
+    console.log('Click onInput');
+    if(this.myInput.length > 5){
+
+    }
+  }
+
+  onCancel(){
+    console.log('Click onCancel');
+  }
+
+
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad BuscapersonaPage');
